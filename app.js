04cf09292c50864044b8998fdc8cf1010c9ac198
Juhász István <istvan.juhasz@telcotrend.hu>
2021-04-09 16:13:40 +0200

    add morgan in order to log postman requests

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orders');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
