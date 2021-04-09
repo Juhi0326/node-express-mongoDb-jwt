cd7d47f7689a11d77f83d585d5b7648feb31bb7b
Juhász István <istvan.juhasz@telcotrend.hu>
2021-04-09 15:33:05 +0200

    create orders route

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const productRoutes = require('./routes/productRoutes')
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orders');
 
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
---
+++ routes/orders.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'this is a get request from order routes'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'this is a post request from order routes'
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    res.status(200).json({
+        message: 'this is a get request from order routes',
+        id: id
+    });
+});
+
+router.patch('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    res.status(201).json({
+        message: 'updated order',
+        id: id
+    })
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    res.status(200).json({
+        message: 'deleted order',
+        id: id
+    })
+});
+
+module.exports = router;
