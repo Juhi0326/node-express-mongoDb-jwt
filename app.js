4de46c29f6370203facfc95a6e99d8deb1b30c9b
Juhász István <istvan.juhasz@telcotrend.hu>
2021-04-12 09:38:58 +0200

    Handling CORS with a middleware

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
