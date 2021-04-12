b362387c1401bb188774227517e30df4b6950b2e
Juhász István <istvan.juhasz@telcotrend.hu>
2021-04-12 09:19:02 +0200

    handling request body (json)

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const productRoutes = require('./routes/productRoutes');
-const orderRoutes = require('./routes/orders');
-const morgan = require('morgan');
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orders");
+const morgan = require("morgan");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use((req,res,next) => {
-    const error = new Error('page not found');
-    error.status = 404;
-    next(error);
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("page not found");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 module.exports = app;
