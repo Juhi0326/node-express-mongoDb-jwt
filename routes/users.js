b54eb2ebb97338ddeaf7e39ccdf7246f7b0352d9
Juhász István <istvan.juhasz@telcotrend.hu>
2021-04-16 09:50:20 +0200

    add login with jwt sign method

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+require('dotenv').config({ path: '../../express-jwt/.env'})
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+
 
 router.post("/signup", (req, res, next) => {
   User.find({ email: req.body.email })
     .exec()
     .then((user) => {
-      if (user.length >= 1 ) {
+      if (user.length >= 1) {
         return res.status(409).json({
           message: "Email is already exist!",
         });
@@ -26,36 +29,81 @@ router.post("/signup", (req, res, next) => {
               password: hash,
             });
             user
-            .save()
-            .then((result) => {
-              res.status(201).json({
-                message: "user created!",
-              });
-            })
-            .catch(err => {
+              .save()
+              .then((result) => {
+                res.status(201).json({
+                  message: "user created!",
+                });
+              })
+              .catch((err) => {
+                console.log(err);
                 res.status(500).json({
-                    Error: err
-                })
-            });
+                  Error: err,
+                });
+              });
           }
         });
       }
     });
 });
 
-router.delete('/:userId', (req, res, next) => {
-    User.deleteOne({_id: req.params.userId})
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
     .exec()
-    .then(result => {
-        res.status(201).json({
-            message: 'User is deleted.'
-        })
+    .then((user) => {
+      if (user < 1) {
+        return res.status(404).json({
+          messages: "Auth failed!",
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(404).json({
+            messages: "Auth failed!",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.secret_JWT_KEY,
+            { expiresIn: "1h" }
+          );
+
+
+          return res.status(200).json({
+            message: "Auth successful",
+            token
+          });
+        }
+        res.status(404).json({
+          messages: "Auth failed!",
+        });
+      });
     })
-    .catch( err => {
-        res.status(500).json({
-            Error: err
-        })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        Error: err,
+      });
+    });
+});
+
+router.delete("/:userId", (req, res, next) => {
+  User.deleteOne({ _id: req.params.userId })
+    .exec()
+    .then((result) => {
+      res.status(201).json({
+        message: "User is deleted.",
+      });
     })
-})
+    .catch((err) => {
+      res.status(500).json({
+        Error: err,
+      });
+    });
+});
 
 module.exports = router;
