25fc5b26d681063181b88126173c0c42a465d369
Juhász István <istvan.juhasz@telcotrend.hu>
2021-04-23 11:21:57 +0200

    outsource file upload to multerStorages.js file

---
+++ multerStorage.js
@@ -0,0 +1,31 @@
+const multer = require('multer');
+
+    const storage = multer.diskStorage({
+        destination: function(req, file, cb) {
+          cb(null, './uploads/');
+        },
+        filename: function(req, file, cb) {
+          cb(null, Date.now() + file.originalname);
+        }
+      });
+      
+      const fileFilter =  (req, file, cb) => {
+    
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        //accept a file
+        cb(null, true);
+        } else {
+        // reject a file
+        cb(null, false);
+        }
+      };
+    
+     upload = multer({
+        storage: storage, 
+        limit: {fileSize: 1024 * 1024 * 5},
+        fileFilter: fileFilter
+    });
+
+    module.exports = {upload};
+
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,36 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const multer = require('multer');
 const adminAuthMiddleware = require('../middleware/authAdmin');
 const userAuthMiddleware = require('../middleware/authUser');
 const productController = require('../controllers/products');
+const {upload} = require('../multerStorage');
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, './uploads/');
-    },
-    filename: function(req, file, cb) {
-      cb(null, Date.now() + file.originalname);
-    }
-  });
-  
-  const fileFilter =  (req, file, cb) => {
-
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    //accept a file
-    cb(null, true);
-    } else {
-    // reject a file
-    cb(null, false);
-    }
-  };
-
-const upload = multer({
-    storage: storage, 
-    limit: {fileSize: 1024 * 1024 * 5},
-    fileFilter: fileFilter
-});
 
 router.get("/", userAuthMiddleware, productController.product_get_all);
 
