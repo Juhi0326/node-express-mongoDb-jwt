d907a8d534502e851ea8e950313869e3b762b7ae
Juhász István <istvan.juhasz@telcotrend.hu>
2021-05-04 17:38:51 +0200

    check product with image in case of delete image

--- controllers/image.js
+++ controllers/image.js
@@ -1,11 +1,12 @@
-const Image = require('../models/image');
-const mongoose = require('mongoose');
-const fs = require('fs');
-const PATH = require('path');
+const Image = require("../models/image");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const PATH = require("path");
+const Product = require("../models/product");
 
 exports.images_get_all = (req, res, next) => {
   Image.find()
-    .select('-_v')
+    .select("-_v")
     .exec()
     .then((docs) => {
       const response = {
@@ -16,8 +17,8 @@ exports.images_get_all = (req, res, next) => {
             imageName: doc.imageName,
             _id: doc._id,
             request: {
-              type: 'GET',
-              url: 'http://localhost:8081/image-setup/' + doc._id,
+              type: "GET",
+              url: "http://localhost:8081/image-setup/" + doc._id,
             },
           };
         }),
@@ -32,7 +33,6 @@ exports.images_get_all = (req, res, next) => {
 };
 
 exports.image_create = (req, res, next) => {
-  console.log(req.file);
   const image = new Image({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
@@ -41,16 +41,15 @@ exports.image_create = (req, res, next) => {
   image
     .save()
     .then((result) => {
-      console.log(result);
       res.status(201).json({
-        message: 'Created new image successfully!',
+        message: "Created new image successfully!",
         createdImage: {
           name: result.name,
           imageName: result.imageName,
           id: result._id,
           request: {
-            type: 'GET',
-            url: 'http://localhost:8081/image-setup/' + result._id,
+            type: "GET",
+            url: "http://localhost:8081/image-setup/" + result._id,
           },
         },
       });
@@ -65,22 +64,22 @@ exports.image_create = (req, res, next) => {
 
 exports.image_get_one_ById = (req, res, next) => {
   const id = req.params.imageId;
+
   Image.findById(id)
-    .select('-__v')
+    .select("-__v")
     .exec()
     .then((doc) => {
       if (doc) {
-        console.log(doc);
         res.status(200).json({
           image: doc,
           request: {
-            type: 'GET',
-            url: 'http://localhost:8081/image-setup',
+            type: "GET",
+            url: "http://localhost:8081/image-setup",
           },
         });
       } else {
         res.status(404).json({
-          message: 'There is not an image with this id!',
+          message: "There is not an image with this id!",
         });
       }
     })
@@ -92,32 +91,53 @@ exports.image_get_one_ById = (req, res, next) => {
 };
 
 exports.image_delete_byId = (req, res, next) => {
+  let productArray = [];
   const id = req.params.imageId;
-  const pathFile = PATH.resolve('');
+  const pathFile = PATH.resolve("");
+
+  try {
+    Product.find()
+      .exec()
+      .then((products) => {
+        products.forEach((product) => {
+          if (product.imageId == id) {
+            productArray.push(product);
+          }
+        });
+      });
+  } catch (error) {
+    console.log(error);
+  }
   Image.findById(id)
     .exec()
     .then((image) => {
       if (!image) {
         return res.status(404).json({
-          messages: 'there is not an image with this id!',
+          messages: "there is not an image with this id!",
+        });
+      }
+      if (productArray.length > 0) {
+        return res.status(404).json({
+          message:
+            "van ilyen kép termékhez társítva, ha törölni szeretnéd a képet, akkor előszőr a terméket kell törölni.",
+          termék: productArray,
         });
       } else {
-        fs.unlink(pathFile + '\\' + image.imageName, (err) => {
+        fs.unlink(pathFile + "\\" + image.imageName, (err) => {
           if (err) {
             console.log(err);
           } else {
-            console.log('sikerült!');
+            console.log("image deleted from the server successfully!");
           }
         });
-
         Image.deleteOne({ _id: id })
           .exec()
           .then((result) => {
             res.status(200).json({
-              message: 'image deleted successfully!',
+              message: "image deleted successfully!",
               request: {
-                type: 'DELETE',
-                url: 'http://localhost:8081/image-setup',
+                type: "DELETE",
+                url: "http://localhost:8081/image-setup",
                 body: { name: image.name, imageName: image.imageName },
                 id: id,
               },
@@ -129,10 +149,5 @@ exports.image_delete_byId = (req, res, next) => {
             });
           });
       }
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        Error: err,
-      });
     });
 };
