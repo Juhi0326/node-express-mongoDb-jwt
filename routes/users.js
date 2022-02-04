89a00a7630afc433b4f97330cbf633fe03afa25f
istvanjuhasz <istvan.juhasz@telcotrend.hu>
2022-02-04 16:44:51 +0100

    delete old signup method

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,6 @@ require('dotenv').config({ path: '../../express-jwt/.env'})
 
 const userController = require('../controllers/users');
 
-
-router.post('/signup', userController.user_signUp);
-
 router.post('/signup2', upload.single('userImage'), userController.user_signUp2);
 
 router.post('/login', userController.user_login);
